2f6b133cac385d753b8e7c403479660ef009340b
Kevin Ellis <kevin.n.ellis3.mil@mail.mil>
2021-08-17 22:46:43 -0400

    Add list_init

--- src/list.c
+++ src/list.c
@@ -4,6 +4,8 @@
 /*
  * @brief element in a list that holds the data
  * @param p_data data the node holds
+ * @param p_prev pointer to the previous element
+ * @param p_next pointer to the next element
  */
 struct elem {
     const void * p_data;
@@ -11,6 +13,14 @@ struct elem {
     struct elem * p_next;
 };
 
+/*
+ * @brief list data type
+ * @param size the number of elements in the list
+ * @param p_destroy user defined function destroy the data in the list
+ * @param p_compare user defined function to compare the data in the list
+ * @param p_head pointer to the first element in the list
+ * @param p_tail pointer to the last element in the list
+ */
 struct list {
     int32_t size;
     void (* p_destroy)(void * p_data);
@@ -19,8 +29,32 @@ struct list {
     elem * p_tail;
 };
 
-// setup
-list * list_init(void (* p_destroy)(void * p_data), void (* p_compare)(const void * p_key1, const void * p_key2));
+// setup functions
+
+/*
+ * @brief creates and initializes a new list
+ * @param p_destroy user defined function destroy the data in the list
+ * @param p_compare user defined function to compare the data in the list
+ * @return pointer to the new list or NULL on error
+ */
+list * list_init(void (* p_destroy)(void * p_data), void (* p_compare)(const void * p_key1, const void * p_key2))
+{
+    // create the list
+    list * p_list = calloc(1, sizeof(*p_list));
+    // if there was an error creating the list then return NULL
+    if (NULL == p_list){
+        perror("List: ");
+        return NULL;
+    }
+    // set the initial values of the list
+    p_list->size = 0;
+    p_list->p_destroy = p_destroy;
+    p_list->p_compare = p_compare;
+    p_list->p_head = NULL;
+    p_list->p_tail = NULL;
+    return p_list;
+}
+
 int8_t list_destroy(list * p_list);
 // adding data
 elem * list_append(list * p_list, const void * p_data);
