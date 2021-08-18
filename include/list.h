b95ca58c87e5462194c5828195e93416e07e1ca6
Kevin Ellis <kevin.n.ellis3.mil@mail.mil>
2021-08-17 22:40:07 -0400

    Rename node to elem to represent an element in a list

--- include/list.h
+++ include/list.h
@@ -1,21 +1,21 @@
 #ifndef _LIST_H
 #define _LIST_H
 #include <stdint.h>
-typedef struct node node;
+typedef struct elem elem;
 typedef struct list list;
 // setup
 list * list_init(void (* p_destroy)(void * p_data), void (* p_compare)(const void * p_key1, const void * p_key2));
 int8_t list_destroy(list * p_list);
 // adding data
-node * list_append(list * p_list, const void * p_data);
-node * list_prepend(list * p_list, const void * p_data);
-node * list_ins_next(list * p_list, node * p_node, const void * p_data);
+elem * list_append(list * p_list, const void * p_data);
+elem * list_prepend(list * p_list, const void * p_data);
+elem * list_ins_next(list * p_list, elem * p_elem, const void * p_data);
 // removing data
-int8_t list_delete(list * p_list, node * p_node);
-int8_t list_rm_next(list * p_list, node * p_node);
-node * list_remove(list * p_list, const void * p_data);
+int8_t list_delete(list * p_list, elem * p_elem);
+int8_t list_rm_next(list * p_list, elem * p_elem);
+elem * list_remove(list * p_list, const void * p_data);
 // getters
-node * list_head(list * p_list);
-node * list_tail(list * p_list);
+elem * list_head(list * p_list);
+elem * list_tail(list * p_list);
 int32_t list_size(list * p_list);
 #endif
--- src/list.c
+++ src/list.c
@@ -1,31 +1,36 @@
 #include <list.h>
 #include <stdint.h>
-struct node {
-    void * p_data;
-    struct node * p_prev;
-    struct node * p_next;
+
+/*
+ * @brief element in a list that holds the data
+ * @param p_data data the node holds
+ */
+struct elem {
+    const void * p_data;
+    struct elem * p_prev;
+    struct elem * p_next;
 };
 
 struct list {
     int32_t size;
     void (* p_destroy)(void * p_data);
     void (* p_compare)(const void * p_key1, const void * p_key2);
-    node * p_head;
-    node * p_tail;
+    elem * p_head;
+    elem * p_tail;
 };
 
 // setup
 list * list_init(void (* p_destroy)(void * p_data), void (* p_compare)(const void * p_key1, const void * p_key2));
 int8_t list_destroy(list * p_list);
 // adding data
-node * list_append(list * p_list, const void * p_data);
-node * list_prepend(list * p_list, const void * p_data);
-node * list_ins_next(list * p_list, node * p_node, const void * p_data);
+elem * list_append(list * p_list, const void * p_data);
+elem * list_prepend(list * p_list, const void * p_data);
+elem * list_ins_next(list * p_list, elem * p_elem, const void * p_data);
 // removing data
-int8_t list_delete(list * p_list, node * p_node);
-int8_t list_rm_next(list * p_list, node * p_node);
-node * list_remove(list * p_list, const void * p_data);
+int8_t list_delete(list * p_list, elem * p_elem);
+int8_t list_rm_next(list * p_list, elem * p_elem);
+elem * list_remove(list * p_list, const void * p_data);
 // getters
-node * list_head(list * p_list);
-node * list_tail(list * p_list);
+elem * list_head(list * p_list);
+elem * list_tail(list * p_list);
 int32_t list_size(list * p_list);
