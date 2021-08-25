37e1cf87d8796010aacdb929ea79d26dba877c12
Kevin Ellis <kevin.n.ellis3.mil@mail.mil>
2021-08-25 18:31:52 -0400

    Add queue header file

---
+++ include/queue.h
@@ -0,0 +1,10 @@
+typedef struct elem member
+typedef struct list queue
+
+void queue_dequeue(queue * p_queue)
+
+#define queue_init list_init
+#define queue_destroy list_destroy
+#define queue_peek list_head
+#define queue_enqueue list_append
+#define queue_size list_size
