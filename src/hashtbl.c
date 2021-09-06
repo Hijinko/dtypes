1c5525605b7997c6e6743ceb9aad207efeebcbaf
Kevin Ellis <kevin.n.ellis3.mil@mail.mil>
2021-09-05 20:43:31 -0400

    Add assing values to hashtble during initialization

--- src/hashtbl.c
+++ src/hashtbl.c
@@ -11,12 +11,14 @@
  * @param p_destroy user defined function to destroy any allocated data
  * @param p_compare user defined function to compare the data in the hashtbl
  *  bin the hashtbl
+ * @param p_table an array that holds the data
  */ 
 struct hashtbl {
     uint64_t buckets;
     uint64_t (* p_hash)(const void * p_key);
-    void (* destroy)(const void * p_data);
-    int8_t (* compare)(const void * p_key1, const void * p_key2);
+    void (* p_destroy)(const void * p_data);
+    int8_t (* p_compare)(const void * p_key1, const void * p_key2);
+    const void * p_table;
 };
 
 /*
@@ -44,6 +46,12 @@ hashtbl * hashtbl_init(uint64_t buckets,
         perror("hashtbl_init ");
         return NULL;
     }
+    // set the values of the hashtbl
+    p_hashtbl->buckets = buckets;
+    p_hashtbl->p_hash = p_hash;
+    p_hashtbl->p_destroy = p_destroy;
+    p_hashtbl->p_compare = p_compare;
+    p_hashtbl->p_table = NULL;
     return p_hashtbl;
 }
 
